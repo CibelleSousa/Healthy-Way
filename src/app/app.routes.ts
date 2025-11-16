9a19cf30ac385481dff8f8e374617b1b92458386
Maria Clara <mcsjesus.cic@uesc.br>
2025-11-16 20:44:41 -0300

    Rotas para o sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Profile } from './pages/profile/profile';
+import { Athletes } from './pages/athletes/athletes';
+import { Settings } from './pages/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'perfil', component: Profile },
+  { path: 'atletas', component: Athletes },
+  { path: 'configuracoes', component: Settings }
+];
