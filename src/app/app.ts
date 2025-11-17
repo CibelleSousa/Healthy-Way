2f73685651f78ac2d6740811a70925cceeabaa7e
davidmarianosantos <david.drinks@hotmail.com>
2025-11-17 00:40:30 -0300

    feat: Criação de card disponibilidade e queixas e correção de dados mocados

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardAtleta } from './card-atleta/card-atleta';
 import { CardList } from './components/athleteList/card-list/card-list';
-import { CardDisponibilidade } from './components/general/card-disponibilidade/card-disponibilidade';
+import { CardDisponibilidade } from './components/athleteProfile/card-disponibilidade/card-disponibilidade';
+import { CardCalorias } from './components/athleteProfile/card-calorias/card-calorias';
+import { CardQueixas } from './components/athleteProfile/card-queixas/card-queixas';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardAtleta, CardList, CardDisponibilidade],
+  imports: [RouterOutlet, CardAtleta, CardList, CardDisponibilidade,CardCalorias,CardQueixas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
