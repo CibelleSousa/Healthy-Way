76a98e277016bb28fa15a31d684325af36ee15bd
CibelleSousa <cibelle.rodrigues@outlook.com>
2025-12-07 04:39:38 -0300

    refactor: reestrutura arquitetura do projeto.

---
+++ backend/backend/src/main/java/com/healthyway/api/HealthywayApiApplication.java
@@ -0,0 +1,13 @@
+package com.healthyway.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthywayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthywayApiApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/healthyway/api/HealthywayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthyway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthywayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
