407dd60f8d81fd05990e58af8e8cee9d06310b24
CibelleSousa <cibelle.rodrigues@outlook.com>
2025-12-07 21:08:11 -0300

    feat: implementa o CRUD para os atletas.

--- backend/src/main/java/com/healthyway/api/service/AthleteService.java
+++ backend/src/main/java/com/healthyway/api/service/AthleteService.java
@@ -27,4 +27,36 @@ public class AthleteService {
         // O 'Optional' é uma caixa que pode ter um Atleta ou estar vazia (se não achar)
         return repository.findById(id); 
     }
+
+    // 5. Método para deletar um atleta pelo ID
+    public void delete(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            // Opcional: Lançar erro se não achar, mas por enquanto vamos só ignorar
+            throw new RuntimeException("Atleta não encontrado para deleção");
+        }
+    }
+
+    // 6. Método para adicionar um atleta novo
+    public Athlete create(Athlete athlete) {
+        // O método .save() do JPA serve tanto para criar quanto para atualizar.
+        // Se o ID for nulo, ele cria. Se tiver ID, ele atualiza.
+        return repository.save(athlete);
+    }
+
+    // 7. Método para atualizar os dados de um atleta
+    public Athlete update(Long id, Athlete atletaComNovosDados) {
+        // Verifica se existe antes de tentar atualizar
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Atleta não encontrado para atualização");
+        }
+        // Garante que o ID do objeto é o mesmo que passamos na URL
+        // (Isso evita que alguém tente mudar o ID do atleta por acidente)
+        atletaComNovosDados.setId(id);
+
+        // Salva (Como o ID já existe no banco, o JPA entende que é um UPDATE)
+        return repository.save(atletaComNovosDados);
+    }
+
 }
